efd95d190874b992d42b36b95d1e2d54cfbb1eca
Wesley Sweazey <sweazey48@gmail.com>
2020-11-14 01:23:19 -0500

    Added TitleScreen

---
+++ Assets/Scripts/SceneManagerScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    public float startTimer = 10.0f;
+    private SceneManager sm = null;
+    public string scenePath;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Countdown());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator Countdown()
+    {
+        float counter = startTimer;
+        while (counter > 0)
+        {
+            yield return new WaitForSeconds(1);
+            counter--;
+        }
+        LoadMenuScene();
+    }
+
+    void LoadMenuScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
