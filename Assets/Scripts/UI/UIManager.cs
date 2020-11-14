1fe8c1715f08fffa8b32a52ba108840ea489b488
Wesley Sweazey <sweazey48@gmail.com>
2020-11-14 17:57:50 -0500

    Adding UIManager and UI Elements

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+	public Canvas[] canvasArray;
+	// Singleton instance.
+	public static UIManager Instance = null;
+
+	// Initialize the singleton instance.
+	private void Awake()
+	{
+		// If there is not already an instance of SoundManager, set it to this.
+		if (Instance == null)
+		{
+			Instance = this;
+		}
+		//If an instance already exists, destroy whatever this object is to enforce the singleton.
+		else if (Instance != this)
+		{
+			Destroy(gameObject);
+		}
+
+		//Set SoundManager to DontDestroyOnLoad so that it won't be destroyed when reloading our scene.
+		DontDestroyOnLoad(gameObject);
+		canvasArray[0].enabled = true;
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+        
+    }
+}
